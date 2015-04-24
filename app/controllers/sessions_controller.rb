68a1d7e48ac7b6501b42b317eaf56c2f4868a649
niliss <siskens@gmail.com>
2015-04-24 17:59:34 -0500

    finish oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,9 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    # Uncomment please
-    # redirect_to root_url
+    redirect_to root_url
   end
-
-
-
+  
   def destroy
     session[:user_id] = nil
     redirect_to root_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'pages/index'
+  root 'pages#index'
+
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
